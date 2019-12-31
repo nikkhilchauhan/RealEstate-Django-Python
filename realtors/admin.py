a0b8bd0756fdd458d28ca7a5ba7ae8c7761a254b
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2019-12-31 19:39:37 +0530

    Customized Django Admin Panel

--- realtors/admin.py
+++ realtors/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import Realtor
 # Register your models here.
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
