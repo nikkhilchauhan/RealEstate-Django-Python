b19687a3911ce0f91587b2175bc268ef414bcf4f
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-01-01 10:54:32 +0530

    Ordered lisitngs by date

--- listings/views.py
+++ listings/views.py
@@ -7,7 +7,7 @@ from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date')
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
