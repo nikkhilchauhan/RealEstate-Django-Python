42704294ab0a34d6b2c295f8ba80a6e2581d0d53
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-01-01 19:20:14 +0530

    Done with Search Form Choices

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
 
 from .models import Listing
 
@@ -26,4 +27,9 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
+from realtors.models import Realtor
+from listings.models import Listing
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 
-from listings.models import Listing
-from realtors.models import Realtor
 
 # Create your views here.
 
@@ -11,7 +12,10 @@ def index(request):
     listings = Listing.objects.order_by(
         '-list_date').filter(is_published=True)[:3]
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
     return render(request, 'pages/index.html', context)
 
