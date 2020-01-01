3336f6c2e55370363740f0dcc1172bd44ce02f9e
Nikhil Chauhan <nikkhil0018@Nikhils-MacBook-Pro.local>
2020-01-01 10:55:25 +0530

    Filtered to show only published listings

--- listings/views.py
+++ listings/views.py
@@ -7,7 +7,7 @@ from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.order_by('-list_date')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
